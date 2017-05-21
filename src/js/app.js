fe22c60bf2d4cd5681fddc9735cd0fa8c096c56a
manuelWeb <devries.webdesigner@gmail.com>
2017-05-21 19:38:23 +0200

    fct avec closure et iief

--- src/js/app.js
+++ src/js/app.js
@@ -8,12 +8,77 @@ var creatBtn = function(lab) {
 
 var villes = ["villeneuve d'ascq", "lille", "cysoing", "Anstaing"];
 
-// sans closure les btn retourne le dernier item Anstaing
+var memoVille = function(index_ary) {
+  return function() {
+    console.log('Votre ville est : ', villes[index_ary]);
+  };
+};
+
 for (var i = 0; i < villes.length; i++) {
   var btn = creatBtn(villes[i]);
-  // console.log('Ta ville : ', villes[i]);
-  btn.onclick = function() {
-    // la val final de i est 4 (villes.length)
-    console.log('Ta ville : ', villes[i - 1]);
+  var logVilleMemo = memoVille(i);
+  btn.onclick = logVilleMemo; // attention pas de () car la fct s'exe au click
+}
+
+// cas setTimeout sans closure
+/*
+for (var i = 0; i <= 3; i++) {
+  var milleSec = 1000 * i;
+  setTimeout(function () {
+    if (i < 3) {
+      console.log(i + 1);
+    } else {
+      console.log("c'est parti !");
+    }
+  }, milleSec);
+} // affiche 4 fois le dernier état
+*/
+
+// avec closure (vers longue) pour memoriser cpt dans memoSec
+//
+// memorisation du contexte i de la bcl for transmi par arg cpt de la <= memoSec
+/*
+var memoSec = function (cpt) {
+  return function () {
+    if (cpt <= 3) {
+      console.log(cpt);
+    } else {
+      console.log("on y est !");
+    }
   };
+};
+
+for (var i = 0; i <= 4; i++) {
+  var milleSec = 1000 * i;
+  setTimeout(memoSec(i), milleSec);
+} // affiche 0 puis 1 puis 2 puis 3 puis on y est !, toutes les sec
+*/
+
+// IEF dans la boucle (mauvaise pratique)
+/*
+for (var i = 0; i < villes.length; i++) {
+  var milleSec = 1000 * i;
+  setTimeout((function(i) {
+    console.log(i); // valeur de i outer
+    return function() {
+      console.log(i); // valeur de (i)
+      console.log(`ville[${i}]: ${villes[i]}`);
+    };
+  })(i), milleSec);
+}
+*/
+
+// IEF en dehors de la boucle
+var cpt = function(index) {
+  console.log("valeur de index outer: ", index); // valeur de index outer
+  return function() {
+    console.log(
+      "valeur de index inner ief: ${index} ",
+      `ville[${index}]: ${villes[index]}` // valeur de  index inner
+    ); 
+  };
+};
+for (var i = 0; i < villes.length; i++) {
+  var milleSec = 1000 * i;
+  setTimeout(cpt(i), milleSec);
 }
